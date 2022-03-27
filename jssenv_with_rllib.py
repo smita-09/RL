7d262e54b4b6cabeb093aff0e52b8a0de1b68476
smita-09 <smita09021996@gmail.com>
2022-03-27 23:32:38 +0200

    Added files

--- jssenv_with_rllib.py
+++ jssenv_with_rllib.py
@@ -6,7 +6,8 @@ warnings.filterwarnings("ignore")
 def run_one_episode (env):
     env.reset()
     sum_reward = 0
-    for i in range(20):
+    for i in range(30):
+        # Just checking what happens if there is no op action is not there =?
         action = env.action_space.sample()
         print("What it is this action", action) # Taking a random action from the sample of actions
         # that is to say from the pool of actions
