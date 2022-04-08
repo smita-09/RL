4e208546965e656ed29f92a605ade04a8c98a4de
smita09 <smita09021996@gmail.com>
2022-04-08 16:24:02 +0200

    updation function to see policy learning behaviour

--- q_update.py
+++ q_update.py
@@ -8,7 +8,6 @@ grads = list([0.01, 0.01, 0.01])
 lr = 0.1
 def update_q_value(actions):
     i = np.random.choice(list(range(len(actions))))
-    # print('This is i: {}, actions[i]: {}, q_vals[i] :{}'.format(i, actions[i], q_vals[i]))
     value_est = q_vals[i] +  np.random.randn() * q_vals[i]
     logits[i] += lr * value_est * grads[i]
     return i, lr*value_est / 10
